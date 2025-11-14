f4da9db1e636e244ce46a15d6203c53820139cb1
Maddy Boss <maddyboss2005@gmail.com>
2025-11-14 10:37:52 -0500

    Completed question 4 of homework 6

--- flask-attempt.py
+++ flask-attempt.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def welcome_nu():
+    return 'Welcome'
+
+@app.route('/user/<username>')
+def user(username):
+    return f'Hello {username}!'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port = 50000)
+    #visit http://127.0.0.1:50000 to see website
